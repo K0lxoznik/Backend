2b571c3e22ab1472bdabe6ceba862bedcfdff3f6
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2023-02-08 10:32:39 +0300

    added morgan

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import morgan from 'morgan';
 import authRouter from './routes/auth';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use('/auth', authRouter);
 
 export default app;
