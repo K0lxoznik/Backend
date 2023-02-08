995b27b668d74f06c2f904991a6cbec8bfd68110
Vladislav Polishchuk <vlad@polisuk.ru>
2023-02-08 11:27:57 +0300

    added db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+import client from './db';
 import authRouter from './routes/auth';
 
 const app = express();
+
+client.connect((err) => {
+	if (err) console.error('connection error', err.stack);
+	else console.log('Connected to database!');
+});
 
 app.use(morgan('dev'));
 app.use('/api/auth', authRouter);
