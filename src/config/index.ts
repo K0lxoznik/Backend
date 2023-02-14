62fd611a8bc1395c5af34e523e8f1c183d7194c9
Vladislav Polishchuk <vlad@polisuk.ru>
2023-02-14 15:37:46 +0300

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,6 @@ const STAGE = process.env.STAGE || 'dev';
 export default {
 	STAGE,
 	PORT: Number(process.env.PORT),
-	DATABASE_HOST: process.env.DATABASE_HOST,
-	DATABASE_PORT: Number(process.env.DATABASE_PORT),
-	DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-	DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-	DATABASE_NAME: process.env.DATABASE_NAME,
+	DATABASE_URL: String(process.env.DATABASE_URL),
 	JWT_SECRET: String(process.env.JWT_SECRET),
 };
