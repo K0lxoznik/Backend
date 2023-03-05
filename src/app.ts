8e66b56f75850cce5ad501057944e238c9bd105b
Vladislav Polishchuk <vlad@polisuk.ru>
2023-03-06 01:16:41 +0300

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { initializeDB } from './db/index';
 import authRouter from './routes/Auth';
 import locationRouter from './routes/Location';
@@ -11,6 +12,7 @@ const app = express();
 
 initializeDB();
 
+app.use(cors({ origin: ['http://localhost:3000'] }));
 app.use(express.json(), morgan('dev'));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
