51b45da25f56eb4aa8645ea92d024b005cae4ad0
Vladislav Polishchuk <vlad@polisuk.ru>
2023-03-06 14:36:30 +0300

    Changed API

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,6 @@ export default {
 	DATABASE_URL: String(process.env.DATABASE_URL),
 	JWT_SECRET: String(process.env.JWT_SECRET),
 	REDIS_URL: String(process.env.REDIS_URL),
+	SEND_EMAIL: String(process.env.SEND_EMAIL),
+	SEND_PASSWORD: String(process.env.SEND_PASSWORD),
 };
